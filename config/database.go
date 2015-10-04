02ba149e7285732694a2dedd0d7526f7072ff78b
Gregory Shilin <gshilin@gmail.com>
2015-10-04 13:30:42 +0300

    to create db and tables only if they doesnt exist

--- config/database.go
+++ config/database.go
@@ -1,40 +1,89 @@
 package config
 
 import (
-	"fmt"
-	r "github.com/dancannon/gorethink"
-	"log"
-	"os"
-	"time"
+  "fmt"
+  r "github.com/dancannon/gorethink"
+  "log"
+  "os"
+  "time"
+  "github.com/Bnei-Baruch/mms-file-manager/logger"
 )
 
-var tables = [...]string{
-	"files",
-}
+var (
+  tables = [...]string{
+    "files",
+  }
+
+  l *log.Logger = logger.InitLogger(&logger.LogParams{LogMode: "screen", LogPrefix: "[DB] "})
+)
 
 func InitDB(dbName string) (session *r.Session, err error) {
 
-	session, err = r.Connect(r.ConnectOpts{
-		Address:  os.Getenv("RETHINKDB_URL"),
-		Database: dbName,
-		MaxIdle:  10,
-		Timeout:  time.Second * 10,
-	})
+  session, err = r.Connect(r.ConnectOpts{
+    Address:  os.Getenv("RETHINKDB_URL"),
+    Database: dbName,
+    MaxIdle:  10,
+    Timeout:  time.Second * 10,
+  })
 
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	if res, err := r.DBCreate(dbName).RunWrite(session); err != nil {
-		log.Println(res, err)
-		if res.Errors > 0 {
-			return nil, fmt.Errorf(res.FirstError)
-		}
-	}
+  if err != nil {
+    l.Println("Connect", err)
+    return
+  }
 
-	for table := range tables {
-		r.DB(dbName).TableCreate(table).RunWrite(session)
-	}
+  r.DBList().Contains(dbName).Do(func(row r.Term) r.Term {
+    return r.Branch(
+      row.Eq(true),
+      nil,
+      r.DBCreate(dbName),
+    )
+  })
+  cursor, err := r.DBList().Contains(dbName).Run(session)
+  if err != nil {
+    return
+  }
+  defer cursor.Close()
 
-	return
+  var dbExists bool
+  cursor.One(&dbExists)
+
+  if !dbExists {
+    if res, err := r.DBCreate(dbName).RunWrite(session); err != nil {
+      l.Printf("Create DB %q issue: (error %v) %#v", dbName, err, res)
+      if res.Errors > 0 {
+        err = fmt.Errorf(res.FirstError)
+      }
+    }
+  } else {
+    l.Printf("WARN Database %q already exists\n", dbName)
+  }
+
+  if err != nil {
+    return
+  }
+
+  for _, table := range tables {
+//    cursor, err := r.TableList().Contains(dbName).Run(session)
+//    if err != nil {
+//      return
+//    }
+//    defer cursor.Close()
+//
+//    var dbExists bool
+//    cursor.One(&dbExists)
+
+
+    res, err := r.DB(dbName).TableCreate(table).RunWrite(session)
+    if err != nil {
+      l.Printf("Create table %q issue: (error %v) %#v", table, err, res)
+      if res.Errors > 0 {
+        return nil, fmt.Errorf(res.FirstError)
+      } else {
+        return nil, err
+      }
+    }
+
+  }
+
+  return
 }
